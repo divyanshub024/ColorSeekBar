976d59eea31c4fcb95366b480bf46bce14f3faea
divyanshu <divyanshub024@gmail.com>
2018-08-18 20:11:05 +0530

    color seekBar demo

--- app/src/main/java/com/divyanshu/colorseekbardemo/MainActivity.kt
+++ app/src/main/java/com/divyanshu/colorseekbardemo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.divyanshu.colorseekbardemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.divyanshu.colorseekbar.ColorSeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        color_seek_bar.setOnColorChangeListener(object: ColorSeekBar.OnColorChangeListener{
+            override fun onColorChangeListener(color: Int) {
+                view.setBackgroundColor(color)
+            }
+
+        })
     }
 }
